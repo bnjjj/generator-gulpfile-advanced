586415dcdab4cc64e6f7ffd24778aeb97939d009
Benjamin Coenen <benjamin.coenen@corp.ovh.com>
2015-09-03 13:58:30 +0200

    refactor (global): create architecture

---
+++ generators/app/index.js
@@ -0,0 +1,11 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+  constructor: function () {
+
+  },
+
+  method1: function () {
+    console.log('Test 1');
+  }
+});
