bbd732a25f855c9ff95f8fd9e4d99302b80e931b
Benjamin Coenen <benjamin.coenen@hotmail.com>
2015-09-17 21:17:45 +0200

    refactor (all): divise into submodules

--- generators/ng-constant/templates/ng-constant.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var gulp = require('gulp');
-var ngConstant = require('gulp-ng-constant');
-
-gulp.task('ng-constant', function () {
-  gulp.src('<%= configFile %>')
-    .pipe(ngConstant())
-    .pipe(gulp.dest('<%= dist %>'));
-});
